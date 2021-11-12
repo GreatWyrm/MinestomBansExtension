a8a557beaf2ac9afa40020f22e229ad9c6a2db2d
GreatWyrm <alecmusante@gmail.com>
2021-11-12 11:21:27 -0500

    We prepare our statements Also, updated Minestom and gradle version

--- src/main/java/com/arcanewarrior/storage/SQLiteStorageIO.java
+++ src/main/java/com/arcanewarrior/storage/SQLiteStorageIO.java
@@ -13,6 +13,9 @@ public class SQLiteStorageIO implements StorageIO {
 
     private final String SQLitePath = "jdbc:sqlite:test.db";
     private final String TABLE_NAME = "BANLIST";
+    private final String uuidFieldName = "UUID";
+    private final String usernameFieldName = "username";
+    private final String banReasonFieldName = "banreason";
     private final int banReasonMaxLength = 100;
 
     @Override
@@ -20,11 +23,10 @@ public class SQLiteStorageIO implements StorageIO {
         try {
             Class.forName("org.sqlite.JDBC");
             Connection connection = DriverManager.getConnection(SQLitePath);
-            String query = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + """
-                              (UUID char(36) PRIMARY KEY NOT NULL,
-                              username varchar(16) NOT NULL,
-                              banreason varchar(""" + banReasonMaxLength + "))";
-            //String query = "CREATE TABLE IF NOT EXISTS BANLIST (UUID char(36) PRIMARY KEY NOT NULL, username varchar(16) NOT NULL, banreason varchar(100))";
+            String query = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME +
+                              "(" + uuidFieldName + " char(36) PRIMARY KEY NOT NULL," +
+                              usernameFieldName + "varchar(16) NOT NULL," +
+                              banReasonFieldName + "varchar(" + banReasonMaxLength + "))";
             // TODO: For backwards compatibility, add ALTER TABLE statements here if the BanDetails gets any more information
             // For example, a ban date
             Statement statement = connection.createStatement();
@@ -45,9 +47,9 @@ public class SQLiteStorageIO implements StorageIO {
             Statement statement = connection.createStatement();
             ResultSet rs = statement.executeQuery(query);
             while(rs.next()) {
-                String id = rs.getString("UUID");
-                String username = rs.getString("username");
-                String banReason = rs.getString("banreason");
+                String id = rs.getString(uuidFieldName);
+                String username = rs.getString(usernameFieldName);
+                String banReason = rs.getString(banReasonFieldName);
                 // Much thanks https://stackoverflow.com/questions/18986712/creating-a-uuid-from-a-string-with-no-dashes
                 UUID uuid = UUID.fromString(id
                         .replaceFirst(
@@ -71,10 +73,12 @@ public class SQLiteStorageIO implements StorageIO {
         }
         try {
             Connection connection = DriverManager.getConnection(SQLitePath);
-            String query = "INSERT INTO " + TABLE_NAME + " (UUID, username, banreason) " +
-                    "VALUES ('" + stripUUID(player.getUuid()) + "','" + player.getUsername() + "','" + reason + "');";
-            Statement statement = connection.createStatement();
-            statement.executeUpdate(query);
+            String queryPrepared = "INSERT INTO " + TABLE_NAME + " (" + uuidFieldName + ", " + usernameFieldName + ", " + banReasonFieldName + ") VALUES (?,?,?)";
+            PreparedStatement statement = connection.prepareStatement(queryPrepared);
+            statement.setString(1, stripUUID(player.getUuid())); // Statements start from 1 >:(
+            statement.setString(2, player.getUsername());
+            statement.setString(3, reason);
+            statement.executeUpdate();
             statement.close();
             connection.close();
         } catch (SQLException e) {
@@ -86,9 +90,10 @@ public class SQLiteStorageIO implements StorageIO {
     public void removeBannedPlayerFromStorage(@NotNull UUID id) {
         try {
             Connection connection = DriverManager.getConnection(SQLitePath);
-            String query = "DELETE from " + TABLE_NAME + " where UUID='" + stripUUID(id) + "';";
-            Statement statement = connection.createStatement();
-            statement.executeUpdate(query);
+            String query = "DELETE from " + TABLE_NAME + " where " + uuidFieldName + "=?;";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, stripUUID(id));
+            statement.executeUpdate();
             statement.close();
             connection.close();
         } catch (SQLException e) {
